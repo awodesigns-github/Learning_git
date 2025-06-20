e1ffcfc146e876fbdc02463f5185056483835d71
joseph <awo.designs1@gmail.com>
2025-06-20 10:18:51 +0300

    Added console.log feature to feature-branch

--- Hello.ts
+++ Hello.ts
@@ -1,2 +1,7 @@
 console.log('Hello Git');
 // Added this line just to understand more on checkout and head
+console.log('First commit before reset');
+
+console.log('Second commit before reset');
+
+// Removed buggy code...
